c089a65b8e3be0cd4233dbca31e362b2695edd44
Johannes <johannes-programming@posteo.org>
2023-11-19 02:31:07 +0100

    u

--- src/lib_dzne_blastn/__init__.py
+++ src/lib_dzne_blastn/__init__.py
@@ -1,54 +1,14 @@
-
-import xml.dom.minidom as _minidom
-
 import lib_dzne_filedata as _fd
+import handle_blastn as _handle_blastn
 
 
-def get_cline(prog, *, query, out, db):
-    return [
-        prog,
-        '-db', db,
-        '-task', 'blastn',
-        '-dust', 'no',
-        '-outfmt', '5',
-        '-max_target_seqs', '1',
-        '-evalue', '0.0001',
-        '-sorthits', '1',
-        '-query', query, 
-        '-out', out,
-    ]
+def get_cline(prog, **kwargs):
+    cline = _handle_blastn.Cline(cmd=prog, **kwargs)
+    return list(cline)
 
 def parse(text):
-    data = _minidom.parseString(text)
-    store = dict()
-    store['query-id'] = _get(data, 'BlastOutput', 'BlastOutput_iterations', 'Iteration', 'Iteration_query-def')
-    store['subject-id'] = _get(data, 'BlastOutput', 'BlastOutput_iterations', 'Iteration', 'Iteration_hits', 'Hit', 'Hit_id')
-    store['bit-score'] = float(_get(data, 'BlastOutput', 'BlastOutput_iterations', 'Iteration', 'Iteration_hits', 'Hit', 'Hsp', 'Hsp_bit-score'))
-    store['evalue'] = float(_get(data, 'BlastOutput', 'BlastOutput_iterations', 'Iteration', 'Iteration_hits', 'Hit', 'Hsp', 'Hsp_evalue'))
-    return _fd.TOMLData(store)
-
-def _get(data, *keys, kind=str):
-    if data is None:
-        return None
-    ans = data
-    try:
-        for key in keys:
-            ans = ans.getElementsByTagName(key)[0]
-        ans = ans.childNodes[0]
-    except ValueError:
-        return float('nan')
-    except IndexError:
-        return float('nan')
-    ans = ans.nodeValue
-    if type(ans) is not str:
-        raise TypeError()
+    summary = _handle_blastn.Summary.from_text(text)
+    data = vars(summary)
+    store = {k.replace('_', '-'):v for k,v in data.items()}
+    ans = _fd.TOMLData(store)
     return ans
-
-
-
-            
-
-
-
-
-
